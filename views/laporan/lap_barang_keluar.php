1861d44356284dd9f4c099b8c1ae68774a6a71c9
sahalassgff <113337692+sahalassgff@users.noreply.github.com>
2024-12-11 16:48:34 +0700

    update detail

--- views/laporan/lap_barang_keluar.php
+++ views/laporan/lap_barang_keluar.php
@@ -82,7 +82,7 @@
                             <th>TANGGAL</th>
                             <th>NAMA BARANG</th>
                             <th>MEREK</th>
-                            <th>KATEGORI</th>
+                            <th>KATEGORI RUANAGAN</th>
                             <th>KETERANGAN</th>
                             <th>JUMLAH</th>
                         </tr>
